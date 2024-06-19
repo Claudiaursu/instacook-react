8d1e96cdef26ec5b6388a75b5042680d9e6a6a03
Claudia Ursu <ursu.claudia99@yahoo.com>
2024-06-19 23:56:26 +0300

    initial edit page

--- src/screens/EditProfile/editProfile.screen.tsx
+++ src/screens/EditProfile/editProfile.screen.tsx
@@ -1,73 +1,127 @@
 import Ionicons from "@expo/vector-icons/Ionicons";
 import type { NativeStackScreenProps } from "@react-navigation/native-stack";
-import React, { useEffect, useState } from "react";
-import { FlatList, SafeAreaView, Switch } from "react-native";
+import React, { useState } from "react";
+import { ScrollView, SafeAreaView, StyleSheet, View } from "react-native";
 import { RootStackParamList } from "../../navigation/navigator.types";
 import { useThemeConsumer } from "../../utils/theme/theme.consumer";
-import { TextInput } from "../../components/text-input";
-import { View, Modal } from 'react-native';
-import { StyleSheet } from "react-native";
-import uuid from 'react-native-uuid';
 import { Button } from "../../components/button";
 import { Text } from "../../components/text";
 import { useDispatch, useSelector } from "react-redux";
-import { updateToken } from "../../store/tokenSlice";
 import { RootState } from "../../store/store";
-import * as ImagePicker from 'expo-image-picker';
-import { useAddNewCollectionMutation, useDeleteCollectionByIdMutation, useGetCollectionsByUserIdQuery } from "../../services/collection.service";
-import { getStorage, ref, uploadBytesResumable } from "firebase/storage";
-import { CollectionNavStackParamList } from "../../components/collection-list-wrapper/collection-list.navigator";
-import { RecipeDto, RecipeSummaryDto } from "../../services/types";
-import RecipeCardComponent from "../../components/recipe-card/recipe-card.component";
-import { useGetRecipesByCollectionIdQuery } from "../../services/recipe.service";
+import { useGetUserByUsernameQuery } from "../../services/user-interaction.service";
 import { ProfileStackParamList } from "../ProfileNavigator/navigator.types";
-
+import { TextInput } from "../../components/text-input";
 
 type ProfileEditProps = NativeStackScreenProps<ProfileStackParamList, "EditProfile">;
 
-const EditProfile = ({ 
-  route,
-  navigation,
-}: {
-  route: any
-  navigation: ProfileEditProps}
-) => {
-
+const EditProfile = ({ route, navigation }: ProfileEditProps) => {
   const { userId } = route.params;
   const dispatch = useDispatch();
   const username = useSelector((state: RootState) => state.userData.username);
-  const loggedId = useSelector((state: RootState) => state.userData.loggedId);
   const token = useSelector((state: RootState) => state.userData.token);
 
-  const { collectionId } = route.params;
+  const { data: userData, refetch: refetchUserData } = useGetUserByUsernameQuery(username);
+  const { theme } = useThemeConsumer();
 
-  const recipesParams = {
-    id: collectionId,
-    token: token
-  }
-  const { data: recipesList, error, isLoading, refetch } = useGetRecipesByCollectionIdQuery(recipesParams);
+  const [formData, setFormData] = useState({
+    nume: userData?.nume || '',
+    prenume: userData?.prenume || '',
+    username: userData?.username || '',
+    parola: userData?.parola || '',
+    email: userData?.email || '',
+    taraOrigine: userData?.taraOrigine || '',
+    telefon: userData?.telefon || '',
+  });
 
-  const {
-      theme
-  } = useThemeConsumer();  
+  const handleInputChange = (field: any, value: any) => {
+    setFormData({ ...formData, [field]: value });
+  };
 
+  const handleSave = () => {
+    //dispatch(updateUserProfile({ ...formData, token }));
+    navigation.goBack();
+  };
 
-    return (
-     <View style={styles.container}> 
-        <Text>hdgsfjsdgfjhdgsfg</Text>
-     </View>
-    )
-}
+  return (
+    <SafeAreaView style={styles.container}>
+      <ScrollView contentContainerStyle={styles.contentContainer}>
+        <View style={styles.formGroup}>
+          <TextInput
+            label="Last Name"
+            value={formData.nume}
+            onChangeText={(text) => handleInputChange('nume', text)}
+          />
+        </View>
+        <View style={styles.formGroup}>
+          <TextInput
+            label="First name"
+            value={formData.prenume}
+            onChangeText={(text) => handleInputChange('prenume', text)}
+          />
+        </View>
+        <View style={styles.formGroup}>
+          <TextInput
+            label="Username"
+            value={formData.username}
+            onChangeText={(text) => handleInputChange('username', text)}
+          />
+        </View>
+        <View style={styles.formGroup}>
+          <TextInput
+            label="Email"
+            value={formData.email}
+            onChangeText={(text) => handleInputChange('email', text)}
+          />
+        </View>
+        <View style={styles.formGroup}>
+          <TextInput
+            label="Country"
+            value={formData.taraOrigine}
+            onChangeText={(text) => handleInputChange('taraOrigine', text)}
+          />
+        </View>
+        <View style={styles.formGroup}>
+          <TextInput
+            label="Phone"
+            value={formData.telefon}
+            onChangeText={(text) => handleInputChange('telefon', text)}
+          />
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button title="Save Changes" onPress={handleSave} />
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button sx = {{backgroundColor: theme.colors.background2}} title="Reset Password" onPress={handleSave} />
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button sx = {{backgroundColor: theme.colors.cardTitle}} title="Delete account" onPress={handleSave} />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    //paddingHorizontal: 5,
-    //paddingVertical: 12,
-    paddingTop: 12,
+    backgroundColor: '#eafafa',
   },
-  listContainer: {
-    justifyContent: 'space-between',
+  contentContainer: {
+    padding: 16,
+  },
+  formGroup: {
+    marginBottom: 16,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    padding: 8,
+    backgroundColor: '#fff',
+  },
+  buttonContainer: {
+    marginTop: 20,
+    alignItems: 'center',
   },
 });
 
